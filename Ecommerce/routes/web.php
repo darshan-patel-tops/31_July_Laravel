93a18cf5ca541998a0458c12af14f9e15f675275
darshan <darshan.patel.tops@gmail.com>
2023-07-31 13:06:40 +0530

    first

--- Ecommerce/routes/web.php
+++ Ecommerce/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Frontend\homecontroller as FrontendHomecontroller;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/',[FrontendHomecontroller::class,'index']);
